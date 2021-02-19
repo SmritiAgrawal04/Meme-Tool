bfe56c8f2a754595a8684ca6272100b20f9de021
SmritiAgrawal04 <smriti.agrawal@students.iiit.ac.in>
2021-02-19 16:45:14 +0530

    final commit

--- Meme/views.py
+++ Meme/views.py
@@ -3,26 +3,35 @@ from .models import Meme
 from django.http import HttpResponse, Http404
 from urllib.parse import urlparse
 # Create your views here.
+
+
+# homepage of the webiste
 def home(request):
     return (render(request, "home.html"))
 
+
 def memes(request):
+    # when a user submits the upload meme request
     if request.method=='POST':
+        # fetch the details entered bythe user
         userName= request.POST['username']
         caption= request.POST['caption']
         imageURL= request.POST['url']
 
+        # save the meme object to the database
         memeObj= Meme()
         memeObj.userName= userName
         memeObj.caption= caption
         memeObj.imageURL= imageURL
-
         memeObj.save()
 
+        # fetch all the memes from database and display 
         memeObj= Meme.objects.filter()        
         return (render(request, 'view.html', {'memes': memeObj}))
 
+    # when a viewer wants to watch memes
     else:
+        # if the mode is mentioned- view/upload
         try:
             mode= request.GET['mode']
             if mode == 'upload':
@@ -36,6 +45,7 @@ def memes(request):
             return (render(request, '404Error.html'))
     
 def specialRequest(request, meme_id):
+    # if the requested id exists render view else 404HTTP
     memeObj= Meme.objects.filter(id=meme_id)   
     if memeObj:
         return (render(request, 'view.html', {'memes': memeObj}))
@@ -43,6 +53,7 @@ def specialRequest(request, meme_id):
         return (render(request, '404Error.html'))
 
 def update(request):
+    # when an update request is made for a  meme
     if request.method == 'GET':
         meme_id= request.GET['meme_id']
         memeObj= Meme.objects.filter(id=meme_id)   
@@ -50,6 +61,7 @@ def update(request):
             return(render(request, "update.html",  {'meme_id': meme_id}))
         else:
             return (render(request, '404Error.html'))
+    # when a user submits update request
     else:
         meme_id= request.POST['meme_id']
         if request.POST['memeCaption'] == 'Yes':
@@ -61,6 +73,7 @@ def update(request):
         return (render(request, 'view.html', {'memes': memeObj}))
 
 
+# increment likes for the meme
 def incrementLikes(request):
     meme_id= request.GET['meme_id']
     currentLikes = Meme.objects.filter(id=meme_id)[0].likes
